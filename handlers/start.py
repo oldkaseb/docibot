5422d5ee77c04b3d175e7ea9bc7557273af83d27
oldkaseb <hamedsocialworks@gmail.com>
2025-08-04 19:22:21 +0400

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,12 +1,27 @@
+from telegram.ext import CommandHandler, CallbackQueryHandler, CallbackContext
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import CommandHandler, CallbackContext
 from utils.db import add_user
 
 def start(update: Update, context: CallbackContext):
     user = update.effective_user
     add_user(user)
-    text = "سلام 👋\nبه ربات رسمی دکتر گشاد خوش اومدی.\nپیامتو ارسال کن به گشادیم برسم زودی جوابتو میدم!"
-    keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("✉️ ارسال پیام به گشاد", callback_data="send_msg")]])
+
+    text = (
+        "سلام 👋\n"
+        "به ربات رسمی دکتر گشاد خوش اومدی.\n"
+        "پیامتو ارسال کن به گشادیم برسم زودی جوابتو میدم!"
+    )
+
+    keyboard = InlineKeyboardMarkup([
+        [InlineKeyboardButton("✉️ ارسال پیام به گشاد", callback_data="send_msg")]
+    ])
+
     update.message.reply_text(text, reply_markup=keyboard)
 
+def send_button_handler(update: Update, context: CallbackContext):
+    query = update.callback_query
+    query.answer()
+    query.message.reply_text("✉️ حالا پیامتو بفرست!")
+
 start_handler = CommandHandler("start", start)
+send_button_handler = CallbackQueryHandler(send_button_handler, pattern="^send_msg$")
