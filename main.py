1175c19e4fdf17dac217a82ff64d87b1963e2c06
oldkaseb <hamedsocialworks@gmail.com>
2025-08-05 00:33:38 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,48 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler, Filters
-from config import BOT_TOKEN
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler, Filters, CallbackContext
+from config import BOT_TOKEN, ADMIN_IDS
 from handlers.message import (
     start_command,
     button_callback,
-    user_message
+    user_message,
+    handle_reply_callback,
+    handle_block_unblock,
+    handle_admin_reply
 )
 from handlers.admin import (
     stats_command,
     help_command,
     forall_command,
     add_admin,
-    remove_admin,
-    handle_reply_callback,
-    handle_block_unblock,
-    handle_admin_reply
+    remove_admin
 )
 
+# شروع ربات
 updater = Updater(token=BOT_TOKEN, use_context=True)
 dp = updater.dispatcher
 
-# دستورات اصلی
+# دستورات کاربری
 dp.add_handler(CommandHandler("start", start_command))
 dp.add_handler(CallbackQueryHandler(button_callback, pattern="^start_message$"))
 dp.add_handler(MessageHandler(Filters.text & ~Filters.command, user_message))
 
-# پاسخ‌دهی ادمین
+# پاسخ ادمین به کاربران (محدود به ادمین‌های تعریف‌شده)
 dp.add_handler(CallbackQueryHandler(handle_reply_callback, pattern="^reply:"))
-dp.add_handler(MessageHandler(Filters.text & Filters.reply, handle_admin_reply))
+dp.add_handler(MessageHandler(
+    Filters.text & Filters.user(user_id=[int(i) for i in ADMIN_IDS.split(',')]),
+    handle_admin_reply
+))
 
 # بلاک و آنبلاک
 dp.add_handler(CallbackQueryHandler(handle_block_unblock, pattern="^(block|unblock):"))
 
-# دستورات ادمین
+# دستورات ویژه ادمین
 dp.add_handler(CommandHandler("stats", stats_command))
 dp.add_handler(CommandHandler("help", help_command))
 dp.add_handler(CommandHandler("forall", forall_command))
 dp.add_handler(CommandHandler("addadmin", add_admin))
 dp.add_handler(CommandHandler("removeadmin", remove_admin))
 
+# اجرای ربات
 updater.start_polling()
 updater.idle()
