07d76933b55fefa42ca4ec54cdcb24a64d6a9b07
oldkaseb <hamedsocialworks@gmail.com>
2025-08-05 00:22:30 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,42 @@
-from telegram.ext import (
-    Updater,
-    CommandHandler,
-    MessageHandler,
-    CallbackQueryHandler,
-    Filters,
-)
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler, Filters
 from config import BOT_TOKEN
 from handlers.message import (
     start_command,
     button_callback,
-    user_message,
-    handle_reply_callback,
-    handle_block_unblock,
-    handle_admin_reply
+    user_message
 )
 from handlers.admin import (
     stats_command,
     help_command,
     forall_command,
     add_admin,
-    remove_admin
+    remove_admin,
+    handle_reply_callback,
+    handle_block_unblock,
+    handle_admin_reply
 )
 
-def main():
-    updater = Updater(token=BOT_TOKEN, use_context=True)
-    dp = updater.dispatcher
+updater = Updater(token=BOT_TOKEN, use_context=True)
+dp = updater.dispatcher
 
-    # 🟢 دستورات کاربران
-    dp.add_handler(CommandHandler("start", start_command))
-    dp.add_handler(CallbackQueryHandler(button_callback, pattern="^start_message$"))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, user_message))
+# دستورات اصلی
+dp.add_handler(CommandHandler("start", start_command))
+dp.add_handler(CallbackQueryHandler(button_callback, pattern="^start_message$"))
+dp.add_handler(MessageHandler(Filters.text & ~Filters.command, user_message))
 
-    # 🟢 پردازش دکمه پاسخ و بلاک/آنبلاک
-    dp.add_handler(CallbackQueryHandler(handle_reply_callback, pattern="^reply:"))
-    dp.add_handler(CallbackQueryHandler(handle_block_unblock, pattern="^(block|unblock):"))
+# پاسخ‌دهی ادمین
+dp.add_handler(CallbackQueryHandler(handle_reply_callback, pattern="^reply:"))
+dp.add_handler(MessageHandler(Filters.text & Filters.reply, handle_admin_reply))
 
-    # 🟢 پاسخ‌گویی ادمین (حالت پاسخ فعال باشد)
-    dp.add_handler(MessageHandler(Filters.text & Filters.reply, handle_admin_reply))
+# بلاک و آنبلاک
+dp.add_handler(CallbackQueryHandler(handle_block_unblock, pattern="^(block|unblock):"))
 
-    # 🟢 دستورات ادمین
-    dp.add_handler(CommandHandler("stats", stats_command))
-    dp.add_handler(CommandHandler("help", help_command))
-    dp.add_handler(CommandHandler("forall", forall_command))
-    dp.add_handler(CommandHandler("addadmin", add_admin))
-    dp.add_handler(CommandHandler("removeadmin", remove_admin))
+# دستورات ادمین
+dp.add_handler(CommandHandler("stats", stats_command))
+dp.add_handler(CommandHandler("help", help_command))
+dp.add_handler(CommandHandler("forall", forall_command))
+dp.add_handler(CommandHandler("addadmin", add_admin))
+dp.add_handler(CommandHandler("removeadmin", remove_admin))
 
-    # ✅ شروع ربات
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+updater.start_polling()
+updater.idle()
