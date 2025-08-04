d6c36c1e16be6d54038e378ce5e5cd55be8815cf
oldkaseb <hamedsocialworks@gmail.com>
2025-08-04 19:30:22 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    CallbackQueryHandler,
+)
 from config import BOT_TOKEN
-from handlers.start import start_handler, send_button_handler
-from handlers.message import user_message_handler, reply_message_handler, button_callback_handler
+
+# هندلرهای هر بخش
+from handlers.start import start_handler, send_button_callback
+from handlers.message import (
+    user_message_handler,
+    reply_message_handler,
+    button_callback_handler,
+)
 from handlers.admin import handlers as admin_handlers
 from handlers.broadcast import forall_init_handler, handle_broadcast_message
 
@@ -9,23 +21,24 @@ def main():
     updater = Updater(BOT_TOKEN, use_context=True)
     dp = updater.dispatcher
 
-    # هندلرهای مربوط به کاربران
+    # ➕ هندلرهای کاربر
     dp.add_handler(start_handler)
-    dp.add_handler(CallbackQueryHandler(send_button_handler, pattern="^send_msg$"))  # دکمه ارسال پیام
+    dp.add_handler(send_button_callback)  # برای دکمه ✉️ ارسال پیام به گشاد
 
-    # هندلرهای مربوط به ارسال و پاسخ پیام
+    # ➕ پیام کاربران و مدیریت پاسخ ادمین
     dp.add_handler(user_message_handler)
     dp.add_handler(reply_message_handler)
     dp.add_handler(button_callback_handler)
 
-    # هندلر پیام همگانی
+    # ➕ پیام همگانی
     dp.add_handler(forall_init_handler)
     dp.add_handler(MessageHandler(Filters.private & ~Filters.command, handle_broadcast_message))
 
-    # دستورات ادمین (help, stats, addadmin, deladmin)
+    # ➕ دستورات ادمینی (help, stats, addadmin, deladmin)
     for handler in admin_handlers:
         dp.add_handler(handler)
 
+    # 🚀 اجرای ربات
     updater.start_polling()
     updater.idle()
 
